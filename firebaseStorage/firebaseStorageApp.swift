106c84d431660a4c06af799cffffb375d8c33338
Berkay <berkaysutlu94@gmail.com>
2023-08-06 15:11:24 +0300

    Initial Commit

---
+++ firebaseStorage/firebaseStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseStorageApp.swift
+//  firebaseStorage
+//
+//  Created by Berkay Sutlu on 6.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct firebaseStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
